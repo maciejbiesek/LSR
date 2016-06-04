b64b9043d580b8c7c2ae515f650a1ea73b2ee6c9
Natalia Postawa <nataliapostawa@users.noreply.github.com>
2016-06-04 13:15:10 +0200

    Add files via upload

--- gaussian_pyramide.py
+++ gaussian_pyramide.py
@@ -15,11 +15,27 @@ for i in range(1,6):
 	G=cv2.pyrUp(G)
 	gpA.insert(i, G)
 
+height=[]
+width=[]
+channels=[]
 
+height, width, channels = gpA.shape
+
+G=gpA[1]
+for i in range(height):
+	for j in range(width):
+            
+		print i, j
+		a=(gpA[1][i][j]+gpA[2][i][j]+gpA[3][i][j]+gpA[4][i][j]+gpA[5][i][j]+gpA[6][i][j])/6
+		G.insert(i, j, a)
+
+cv2.imwrite('pyramide_all.png', G)		            
+            
+'''
 cv2.imwrite('pyramide0.png', gpA[0])
 cv2.imwrite('pyramide1.png', gpA[1])
 cv2.imwrite('pyramide2.png', gpA[2])
 cv2.imwrite('pyramide3.png', gpA[3])
 cv2.imwrite('pyramide4.png', gpA[4])
 cv2.imwrite('pyramide5.png', gpA[5])
-	
+'''
